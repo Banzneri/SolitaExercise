78cda3fef3f2f814a609d714a7ea682f5409b4d2
Banzneri <pasi.hanhijarvi@gmail.com>
2021-12-16 23:15:40 +0200

    modify eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   ],
   rules: {
     'no-console': 'off',
+    'import/extensions': 'always'
   },
 };
--- server/temp/csvTest.js
+++ server/temp/csvTest.js
@@ -1,11 +1,11 @@
 import path from 'path';
-import CSVToArray from './lib/CSVToArray';
+import CSVToArray from '../lib/CSVToArray.js';
 
 const dirname = path.resolve();
 
 const farmName = process.argv.splice(2)[0] || 'nooras_farm';
 
-const filePath = path.join(dirname, 'initial_data', `${farmName}.csv`);
+const filePath = path.join(dirname, '..', 'initial_data', `${farmName}.csv`);
 
 const handleResult = (error, result) => {
   if (error) {
