6f88c782b5983dcfb9f53d91f0f1b148b4ceb324
Banzneri <pasi.hanhijarvi@gmail.com>
2021-12-17 09:56:56 +0200

    add farm data validation

--- server/lib/utils.ts
+++ server/lib/utils.ts
@@ -4,8 +4,30 @@ import { FarmDataObject } from '../types/FarmDataObject';
 import { HandleCSVParsingResults } from '../types/HandleCSVParsingResults';
 
 export const validFarmDataObject = (farmData: FarmDataObject) => {
-  const data = farmData;
-  return data;
+  const validTemperature = (temperature: number) => {
+    const isValid = temperature >= -50 && temperature <= 100;
+    return isValid;
+  };
+
+  const validRainfall = (rainFall: number) => {
+    const isValid = rainFall >= 0 && rainFall <= 500;
+    return isValid;
+  };
+
+  const validPh = (ph: number) => {
+    const isValid = ph >= 0 && ph <= 14;
+    return isValid;
+  };
+
+  const { sensorType, value } = farmData;
+
+  switch (sensorType.toLowerCase()) {
+    case 'temperature': return validTemperature(value);
+    case 'rainfall': return validRainfall(value);
+    case 'ph': return validPh(value);
+    default:
+      return false;
+  }
 };
 
 export const CSVToArray = (
