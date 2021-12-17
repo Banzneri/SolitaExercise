c6d88e2ae3d9119879693b50db884b17e4f1df54
Banzneri <pasi.hanhijarvi@gmail.com>
2021-12-17 02:19:13 +0200

    convert project to typescript

--- server/temp/csvTest.ts
+++ server/temp/csvTest.ts
@@ -1,13 +1,16 @@
 import path from 'path';
-import CSVToArray from '../lib/CSVToArray.js';
-
-const dirname = path.resolve();
+import CSVToArray from '../lib/CSVToArray';
+import { FarmDataObject } from '../types/FarmDataObject';
 
 const farmName = process.argv.splice(2)[0] || 'nooras_farm';
 
+const dirname = path.resolve();
 const filePath = path.join(dirname, '..', 'initial_data', `${farmName}.csv`);
 
-const handleResult = (error, result) => {
+const handleResult = (
+  error: Error | null,
+  result?: Array<FarmDataObject>,
+) => {
   if (error) {
     console.log(error.message);
   }
