fbf2c19e71d49188f68a53b6fbd4d53973d964bb
Banzneri <pasi.hanhijarvi@gmail.com>
2022-01-05 11:12:19 +0200

    add requests.rest file for testing routes

--- server/app/server.ts
+++ server/app/server.ts
@@ -1,11 +1,14 @@
-import express, { Application } from 'express';
+import express from 'express';
+import routes from '../routes/routes';
 
-const app: Application = express();
+const app = express();
 
 const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+routes(app);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
--- server/routes/farmDataRoutes.ts
+++ server/routes/farmDataRoutes.ts
@@ -3,7 +3,7 @@ import * as farmDataController from '../controllers/FarmDataController';
 
 const farmDataRoutes = (app: Application) => {
   app.get('/farmData', farmDataController.getAllFarmData);
-  app.get('/farmData/id/:id', farmDataController.getFarmDataByFarmId);
+  app.get('/farmData/farmId/:farmId', farmDataController.getFarmDataByFarmId);
 };
 
 export default farmDataRoutes;
