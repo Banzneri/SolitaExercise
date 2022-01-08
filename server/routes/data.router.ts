1acbd89dedb0ae39d29126dd45a8d23bc5648f99
Banzneri <pasi.hanhijarvi@gmail.com>
2022-01-08 17:53:09 +0200

    minor fix to routes

--- server/routes/data.router.ts
+++ server/routes/data.router.ts
@@ -3,10 +3,30 @@ import * as controller from '../controllers/farm.data.controller';
 
 const dataRouter = express.Router({ mergeParams: true });
 
-dataRouter.get('/', controller.validate('getDataByFarmId'), controller.getDataByFarmId);
-dataRouter.get('/sensor/:sensor', controller.validate('getDataByFarmIdAndSensor'), controller.getDataByFarmIdAndSensor);
-dataRouter.get('/sensor/:sensor/average', controller.validate('getAllTimeAverage'), controller.getAllTimeAverage);
-dataRouter.get('/sensor/:sensor/min-max', controller.validate('getAllTimeMinMax'), controller.getAllTimeMinMax);
-dataRouter.get('/timespan/:startDate&:endDate/sensor/:sensor', controller.validate('getSensorDataBetweenDates'), controller.getSensorDataBetweenDates);
+dataRouter.get(
+  '/',
+  controller.validate('getDataByFarmId'),
+  controller.getDataByFarmId,
+);
+dataRouter.get(
+  '/sensor/:sensor',
+  controller.validate('getDataByFarmIdAndSensor'),
+  controller.getDataByFarmIdAndSensor,
+);
+dataRouter.get(
+  '/sensor/:sensor/average',
+  controller.validate('getAllTimeAverage'),
+  controller.getAllTimeAverage,
+);
+dataRouter.get(
+  '/sensor/:sensor/min-max',
+  controller.validate('getAllTimeMinMax'),
+  controller.getAllTimeMinMax,
+);
+dataRouter.get(
+  '/timespan/:startDate&:endDate/sensor/:sensor',
+  controller.validate('getSensorDataBetweenDates'),
+  controller.getSensorDataBetweenDates,
+);
 
 export default dataRouter;
--- server/routes/month.router.ts
+++ server/routes/month.router.ts
@@ -3,9 +3,25 @@ import * as controller from '../controllers/farm.data.controller';
 
 const monthRouter = express.Router({ mergeParams: true });
 
-monthRouter.get('/', controller.validate('getAllMonthlyData'), controller.getAllMonthlyData);
-monthRouter.get('/sensor/:sensor', controller.validate('getMonthlyData'), controller.getMonthlyData);
-monthRouter.get('/sensor/:sensor/average', controller.validate('getMonthlyAverage'), controller.getMonthlyAverage);
-monthRouter.get('/sensor/:sensor/min-max', controller.validate('getMonthlyMinMax'), controller.getMonthlyMinMax);
+monthRouter.get(
+  '/',
+  controller.validate('getAllMonthlyData'),
+  controller.getAllMonthlyData,
+);
+monthRouter.get(
+  '/sensor/:sensor',
+  controller.validate('getMonthlyData'),
+  controller.getMonthlyData,
+);
+monthRouter.get(
+  '/sensor/:sensor/average',
+  controller.validate('getMonthlyAverage'),
+  controller.getMonthlyAverage,
+);
+monthRouter.get(
+  '/sensor/:sensor/min-max',
+  controller.validate('getMonthlyMinMax'),
+  controller.getMonthlyMinMax,
+);
 
 export default monthRouter;
