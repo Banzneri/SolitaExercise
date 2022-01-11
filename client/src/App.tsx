bb893cb62a4b18d0bd5d6cddf6626e3a4855397f
Banzneri <pasi.hanhijarvi@gmail.com>
2022-01-11 12:46:56 +0200

    fix import path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FarmData } from './types/types';
-import DataTable from './components/DataTable';
+import DataTable from './components/data-table/DataTable';
 import { getDataByFarmIdAndSensor } from './services/DataService';
 
 const App = () => {
