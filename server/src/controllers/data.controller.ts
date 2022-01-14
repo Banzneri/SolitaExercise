6469726d17d00e0bb77d567f188cd53cdc15442e
Banzneri <pasi.hanhijarvi@gmail.com>
2022-01-14 10:22:50 +0200

    minor refactor of data controller

--- server/src/controllers/data.controller.ts
+++ server/src/controllers/data.controller.ts
@@ -9,14 +9,14 @@ const handleRequest = async (
   handle: Function,
   params: Array<any>,
 ) => {
-  const page = req.query.page
-    ? Number(req.query.page)
-    : 1;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return respondError(res, 400, errors);
   }
   try {
+    const page = req.query.page
+      ? Number(req.query.page)
+      : 1;
     const results = await handle(...params, page);
     return respondResults(res, results);
   } catch (error) {
